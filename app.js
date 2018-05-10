9bd84a133b5402d932fb3e7b7d9ac66d36007318
Zhenzhen You <swimreally@MacBook-Air.local>
2018-05-10 11:59:46 -0700

    change createNewEvent to promise, and allow it to grasp info from completeSentence

--- app.js
+++ app.js
@@ -21,7 +21,12 @@ app.startQuestion = (closeConnectionCallback) => {
     const continueCallback = () => app.startQuestion(closeConnectionCallback);
 
     if (res.action === 'Complete a sentence') app.completeSentence(continueCallback);
-    if (res.action === 'Create a new user') app.createNewUser(continueCallback);
+    if (res.action === 'Create a new user') 
+      app.createNewUser().then((result) => {
+          console.log('Your info has been saved to the database.');
+        }).catch((err) => {
+          console.log(err);
+        }).then(() => continueCallback());
     if (res.action === 'Find one event of a particular type in San Francisco next week') app.searchEventful(continueCallback);
     if (res.action === 'Mark an existing user to attend an event in database') app.matchUserWithEvent(continueCallback);
     if (res.action === 'See all events that a particular user is going to') app.seeEventsOfOneUser(continueCallback);
@@ -33,62 +38,67 @@ app.startQuestion = (closeConnectionCallback) => {
   })
 }
 
+const getUserName = () => {
+  return inquirer.prompt([{
+    type: 'input',
+    message: 'What is your name?',
+    name: 'name'
+  }, {
+    type: 'input',
+    message: 'What is your email?',
+    name: 'email'
+  }]);
+};
+
+const promisedQuery = (query, arg) => {
+  return new Promise((resolve, reject) => {
+    connection.query(query, arg, function(err, result, field) {
+      if (err) {
+        reject(err);
+      }
+      resolve(result);
+    });
+  });
+};
+
 let username;
 let useremail;
 
 app.completeSentence = (continueCallback) => {
-  inquirer.prompt([{
-    type: 'input',
-    message: 'What is your name?',
-    name: 'name'
-  }, {
-    type: 'input',
-    message: 'What is your email?',
-    name: 'email'
-  }]).then((res) => {
-      username = res.name;
-      useremail = res.email;
-      console.log('Your name is: ' + username + '. Your email is: ' + useremail + '.');
-      continueCallback();
-    }).catch((err) => {
-      console.log(err);
-    })
+  getUserName().then((res) => {
+    username = res.name;
+    useremail = res.email;
+    console.log('Your name is: ' + username + '. Your email is: ' + useremail + '.');
+    continueCallback();
+  }).catch((err) => {
+    console.log(err);
+  })
 }
 
-app.createNewUser = (continueCallback) => {
-  inquirer.prompt([{
-    type: 'input',
-    message: 'What is your name?',
-    name: 'name'
-  }, {
-    type: 'input',
-    message: 'What is your email?',
-    name: 'email'
-  }]).then((res) => {
-      username = res.name;
-      useremail = res.email;
+app.createNewUser = () => {
+  let newuser = {};
 
-      console.log('Your name is: ' + username + '. Your email is: ' + useremail + '.');
+  if (username && useremail) {
+    newuser.name = username;
+    newuser.email = useremail;  
+    return promisedQuery('INSERT INTO users SET ?', newuser);
+  }
 
-      const newuser = {name: username, email: useremail};
-
-      connection.query('INSERT INTO users SET ?', newuser, function (err, result, field) {
-        if (err) throw err;
-        console.log("1 user " + username + " inserted into mySQL database.");
-      });
-      continueCallback();
-    }).catch((err) => {
-      console.log(err);
-    })
+  return getUserName().then((res) => {
+    newuser.name = username = res.name;
+    newuser.email = useremail = res.email;      
+    console.log('Your name is: ' + username + '. Your email is: ' + useremail + '.');
+    return promisedQuery('INSERT INTO users SET ?', newuser);
+  })
 }
 
 app.searchEventful = (continueCallback) => {
-  inquirer.prompt({
+  return inquirer.prompt({
     type: 'input',
     message: 'What kind of event do you want to search?',
     name: 'keyword'
   }).then((res) => {
-    console.log("You are searching for " + res.keyword + ".");
+    console.log('You are searching for ' + res.keyword + '.');
 
     eventSearch(res.keyword, (newEvent) => {
       inquirer.prompt({
@@ -96,16 +106,18 @@ app.searchEventful = (continueCallback) => {
         message: 'Do you want to save this event? Y/N',
         'default': false,
         name: 'saveToDB'
-      }).then((res) => {
-          if (res.saveToDB === true) {
-            connection.query('INSERT INTO events SET ?', newEvent, function (err, result, field) {
-              if (err) throw err;
-            });
-            console.log("1 Event inserted into mySQL database.");
+      })
+      .then((res) => {
+        if (res.saveToDB === true) {
+          connection.query('INSERT INTO events SET ?', newEvent, function (err, result, field) {
+            if (err) throw err;
+            console.log('Your event has been saved to the database.');
+            // console.log(result.insertId);
             continueCallback();
-          } 
-          else {
-            app.searchEventful(continueCallback);
+          })
+        } 
+        else {
+          app.searchEventful(continueCallback);
         }
       })
       .catch(err => {
